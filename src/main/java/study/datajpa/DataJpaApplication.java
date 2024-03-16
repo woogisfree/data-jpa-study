14181738bc10aebbe14f87e8ee0d5997dda7fc66
woogie <woogisfree@gmail.com>
2024-03-16 22:15:09 +0900

    Auditing - Spring Data JPA

--- src/main/java/study/datajpa/DataJpaApplication.java
+++ src/main/java/study/datajpa/DataJpaApplication.java
@@ -2,12 +2,24 @@ package study.datajpa;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+import java.util.Optional;
+import java.util.UUID;
+
+@EnableJpaAuditing
 @SpringBootApplication
 public class DataJpaApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DataJpaApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(DataJpaApplication.class, args);
+    }
 
+    @Bean
+    public AuditorAware<String> auditorProvider() {
+        // 실무에서는 session, security 에서 꺼내서 넣어야함
+        return () -> Optional.of(UUID.randomUUID().toString());
+    }
 }
--- src/main/java/study/datajpa/entity/Member.java
+++ src/main/java/study/datajpa/entity/Member.java
@@ -15,7 +15,7 @@ import static jakarta.persistence.FetchType.*;
         query = "select m from Member m where m.username = :username"
 )
 @NamedEntityGraph(name = "Member.all", attributeNodes = @NamedAttributeNode("team"))
-public class Member extends JpaBaseEntity {
+public class Member extends BaseTimeEntity {
 
     @Id
     @GeneratedValue
